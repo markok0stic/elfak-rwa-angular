a3f4218439e521eca595738da069bfe4cd0ca99e
Mare Kostic <marko.k0stic@elfak.rs>
2023-01-12 09:44:19 +0100

    Update home.component.ts

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
+  templateUrl: '../calendar/home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
